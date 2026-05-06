6f0f744b919a727585fd61ee61afb92405a77d7c
Lokesh0018 <munakalalokesh222@gmail.com>
2026-05-06 16:20:45 +0530

    feat(components): integrate authentication views

---
+++ src/components/logins/index.ts
@@ -0,0 +1,7 @@
+export * from "./Login1/Login1";
+export * from "./Login2/Login2";
+export * from "./Login3/Login3";
+export * from "./Login4/Login4";
+export * from "./Login5/Login5";
+export * from "./Login6/Login6";
+export * from "./Login7/Login7";
